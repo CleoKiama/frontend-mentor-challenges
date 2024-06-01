9f15c6ef975b9de0b81859c354e2133d90be7d47
CleoKiama <cleophaskiama7@gmail.com>
2024-06-01 22:17:52 +0300

    added the carts counter

--- E-commerce-product-page/src/components/cartPopover.tsx
+++ E-commerce-product-page/src/components/cartPopover.tsx
@@ -1,11 +1,29 @@
 import * as Popover from '@radix-ui/react-popover';
 import './styles/cartStyles.css';
 import Cart from './cart';
+import { useData } from '../App';
 
-const CartPopover = () => (
+const CartPopover = () =>{
+  const dataRef = useData()
+  let quantity = 0
+  if(dataRef) {
+  quantity = dataRef?.data.cart.length
+  dataRef!.data.cart.forEach(item=>{
+    if(item.quantity > 0) quantity += item.quantity
+  })
+  }
+ return (
   <Popover.Root >
     <Popover.Trigger asChild>
-    <img src="/icon-cart.svg" className="h-18 w-18 ml-auto mr-4" />
+      <div className='flex flex-row items-center ml-auto mr-4'>
+    <img src="/icon-cart.svg" className="h-18 w-18" />
+      {
+      dataRef!.data.cart.length > 0 &&
+    <p className='text-white bg-orange-500 w-4 h-4 self-start mb-3  text-center rounded-[100px] px-[1px] text-xs'>
+    {quantity}
+    </p>
+      }
+      </div>
     </Popover.Trigger>
     <Popover.Portal>
       <Popover.Content className="PopoverContent" align="end" alignOffset={-5}  side="bottom" sideOffset={20}>
@@ -15,5 +33,6 @@ const CartPopover = () => (
   </Popover.Root>
 );
 
+} 
 export default CartPopover;
 
